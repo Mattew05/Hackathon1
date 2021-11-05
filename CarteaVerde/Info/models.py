66e628fbd87c3d62725b483403fad35b3a7c3f0e
Mattew05 <mateinr5@yahoo.ro>
2021-11-05 17:09:59 +0200

    posts

--- CarteaVerde/Info/models.py
+++ CarteaVerde/Info/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
