9191e694d0970da3124cbe49d5a1eb7c337ecb6e
Mattew05 <mateinr5@yahoo.ro>
2021-11-05 20:41:00 +0200

    frontpage

--- CarteaVerde/User/models.py
+++ CarteaVerde/User/models.py
@@ -6,7 +6,7 @@ class user(models.Model):
     id=models.AutoField(auto_created = True,primary_key = True,)
     nume=models.CharField(max_length=200)
     email=models.CharField(max_length=200)
-    imagine=models.ImageField(default="media/user/default.jpeg")
+    imagine=models.ImageField(default="media/user/default.jpeg",upload_to='media/user')
     locatie=models.CharField(max_length=200)
     date=models.TextField()
     email=models.CharField(max_length=200)
