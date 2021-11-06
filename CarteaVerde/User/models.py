4b1073a703e8916a4b8b77703eacda6219b6886f
doilonar <r_damoc@yahoo.com>
2021-11-06 10:42:41 +0200

    user

--- CarteaVerde/User/models.py
+++ CarteaVerde/User/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class user(models.Model):
-    id=models.AutoField(auto_created = True,primary_key = True,)
+    id=models.AutoField(auto_created = True,primary_key = True)
     nume=models.CharField(max_length=200)
-    email=models.CharField(max_length=200)
-    imagine=models.ImageField(default="media/user/default.jpeg",upload_to='media/user')
+    #imagine=models.ImageField(upload_to="media/user",default="media/user/default.jpeg")
     locatie=models.CharField(max_length=200)
     date=models.TextField()
     email=models.CharField(max_length=200)
-    parola=models.CharField(max_length=200)
+    password=models.CharField(max_length=200)
+    USERNAME_FIELD = ''
